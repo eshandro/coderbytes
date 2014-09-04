9341cb9b2cb3957a56a54c959226e88f9fada398
eshandro <erik843@gmail.com>
2014-09-04 09:56:09 -0600

    One medium challenge done and formatted division refactored

--- formattedDivision.js
+++ formattedDivision.js
@@ -4,9 +4,8 @@ function formattedDivision(num1,num2) {
   var leftSide = [];
   var rightSide = [];
   answerList = answerList.split('.');
-  if (answerList.length === 1) answerList.push('');
+  if (answerList.length === 1) rightSide.push('.0000');
   var count = 0;
-  // if (answerList[0].length =0) leftSide.push('0'); 
   for (var i = answerList[0].length-1; i >=0; i--) {
     leftSide.unshift(answerList[0].charAt(i));
     count++;
@@ -15,18 +14,42 @@ function formattedDivision(num1,num2) {
       count = 0;
     }
   }
-  rightSide.push('.');
-  if 
-  for (var j = 0; j < answerList[1].length; j++) {
-    if (answerList[1].length === 0) { 
-      rightSide.push('0000');
-      break;
+  
+  if (answerList[1]) {
+    rightSide.push('.');
+    for (var j = 0; j < answerList[1].length; j++) {
+      if (answerList[1].length === 0) { 
+        rightSide.push('0000');
+        break;
+      }
+      if (j > 3) break;
+      rightSide.push(answerList[1].charAt(j));
     }
-    if (j > 3) break;
-    rightSide.push(answerList[1].charAt(j));
   }
   leftSide = leftSide.join('');
   rightSide = rightSide.join('');
-  return leftSide + rightSide;
-                   
+  return leftSide + rightSide;                   
+}
+
+// 2nd way to do using toFixed
+function formattedDivision2(num1, num2) {
+  var dividedNum = num1 / num2;
+  dividedNum = dividedNum.toFixed(4);
+  console.log(dividedNum);
+  var leftSide = dividedNum.substring(0, dividedNum.indexOf('.'));
+  leftSide = leftSide.split('');
+  console.log(leftSide);
+  var count = 0;
+  var tempList = [];
+  for (var i = leftSide.length-1; i >=0; i--) {
+    tempList.unshift(leftSide[i]);
+    count++;
+    if (count === 3) {
+      tempList.unshift(',');
+      count = 0;
+    }
+  }
+  leftSide = tempList.join('');
+  return leftSide + dividedNum.substring(dividedNum.indexOf('.'));
+
 }
