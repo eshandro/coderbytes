27014e57a6f44670570a80680a24695747a5ae95
eshandro <erik843@gmail.com>
2014-09-06 09:55:56 -0600

    Update and understand findPermutations

--- findPermuations.js
+++ findPermuations.js
@@ -20,17 +20,27 @@ function convertToArr(input) {
 function findPermutations(inputArr) {
 	var results = [];
 	function permute(arr,data) {
-		console.log('permute called');
-		console.log('arr passed: ', arr)
+		console.log('permute called and arr passed: ', arr);
+		console.log('permute called and data passed:', data);
 		var current;
 		var temp = data || [];
 
 		for (var i = 0; i < arr.length; i++) {
+			// remove item in arr at index i and set = to current
 			current = arr.splice(i,1)[0];
-			console.log('current: ', current);
-			console.log('arr: ', arr)
-			if (arr.length === 0) results.push(temp.concat([current]));
+			console.log('current after splice and i is: ', current + ' ' + i);
+			console.log('arr after splice: ', arr)
+			// If arr.length = 0, push the concat of temp and [current] to results
+			if (arr.length === 0) {
+				results.push(temp.concat([current]));
+				console.log('pushed to results: ', temp.concat([current]));
+			}
+			console.log('arr.slice(): ', arr.slice());
+			console.log('concat temp and current: ', temp.concat([current]));
+			// Calls permute passing in the arr and the concat of temp and 
+			// [current] as data
 			permute(arr.slice(), temp.concat([current]));
+			// Adds current into arr at position 1
 			arr.splice(i, 0, current);
 		}
 		return results;
